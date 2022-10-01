a55550730a3c180e5e7d4be27ad5c50441225efe
SahilNajafov <101974628+SahilNajafov@users.noreply.github.com>
2022-10-02 00:28:55 +0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if(number==0){
+            System.out.println(0);
+        }
+        else if(number%2==0){
+            if(number>0){
+                System.out.println("Müsbət cüt ədəddir");
+            }
+            else{
+                System.out.println("Mənfi cüt ədəddir");
+            }
+        }
+        else{
+            if(number>0){
+                System.out.println("Müsbət tək ədəddir");
+            }
+            else{
+                System.out.println("Mənfi tək ədəddir");
+            }
+        }
+
+
     }
 }
