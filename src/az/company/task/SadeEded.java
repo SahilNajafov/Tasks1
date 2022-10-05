8dece82d4475349af0bebf309e5d44ff70f5cdd7
SahilNajafov <101974628+SahilNajafov@users.noreply.github.com>
2022-10-05 20:33:33 +0400

    update5

--- src/az/company/task/SadeEded.java
+++ src/az/company/task/SadeEded.java
@@ -12,19 +12,25 @@ public class SadeEded {
 
         int i = 1;
         int b = 0;
-        while (i <= (num / 2) && num>0) {
+        while (i <= (num / 2) && num > 0) {
             if (num % i == 0) {
                 b++;
             }
             i++;
         }
-        if (num == 1) {
-            System.out.println("Ədəd nə sadə, nə də mürəkkəbdir");
-        } else if (b == 1) {
-            System.out.println("Sadə ədəddir");
-        } else {
-            System.out.println("Sadə ədəd deyil");
+        if (num > 0) {
+            if (num == 1) {
+                System.out.println("Ədəd nə sadə, nə də mürəkkəbdir");
+            } else if (b == 1) {
+                System.out.println("Sadə ədəddir");
+            } else {
+                System.out.println("Sadə ədəd deyil");
+            }
         }
+        else{
+            System.out.println("Ədədi yenidən daxil edin");
+        }
+
 
     }
 }
