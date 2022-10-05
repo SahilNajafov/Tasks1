fbe0e77842ad26b4f9c398547f59f3050a69d91c
SahilNajafov <101974628+SahilNajafov@users.noreply.github.com>
2022-10-05 20:26:01 +0400

    update4

---
+++ src/az/company/task/SadeEded.java
@@ -0,0 +1,30 @@
+package az.company.task;
+
+import java.util.Scanner;
+
+public class SadeEded {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ədədi daxil edin: ");
+        int num = scanner.nextInt();
+
+        int i = 1;
+        int b = 0;
+        while (i <= (num / 2)) {
+            if (num % i == 0) {
+                b++;
+            }
+            i++;
+        }
+        if (num == 1) {
+            System.out.println("Ədəd nə sadə, nə də mürəkkəbdir");
+        } else if (b == 1) {
+            System.out.println("Sadə ədəddir");
+        } else {
+            System.out.println("Sadə ədəd deyil");
+        }
+
+    }
+}
