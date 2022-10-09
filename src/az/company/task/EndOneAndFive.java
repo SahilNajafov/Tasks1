61150cc001abe294be6a24d457f4afdd8c6708cc
SHAIG JAFAROV <shaiq.jafarov@abb-bank.az>
2022-10-09 10:42:43 +0400

    correction

--- src/az/company/task/EndOneAndFive.java
+++ src/az/company/task/EndOneAndFive.java
@@ -1,6 +1,6 @@
 package az.company.task;
 
-public class Endoneandfive {
+public class EndOneAndFive {
     public static void main(String[] args) {
         for (int i = 1; i < 10000; i++) {
             if (i % 10 == 1 || i % 10 == 5) {
