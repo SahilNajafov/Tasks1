5172e415709565ea8dee57179f16fe923dcc5d74
SHAIG JAFAROV <shaiq.jafarov@abb-bank.az>
2022-10-30 11:10:26 +0400

    correction

--- src/az/company/task/lesson15/FindCommonCharacters.java
+++ src/az/company/task/lesson15/FindCommonCharacters.java
@@ -2,27 +2,27 @@ package az.company.task.lesson15;
 
 public class FindCommonCharacters {
     public static void main(String[] args) {
-        String[] words = new String[]{"cool","lock","cook"};
-//        for (String g:FindingCommonLetter(words)) {
-//
-//        }
-//        System.out.print(g+" ");
-        System.out.println(FindingCommonLetter(words));
+        String[] words = new String[]{"cool", "lock", "cook"};
+        for (char as : findingCommonLetter(words)) {
+            System.out.println(as);
+        }
+//        System.out.println(findingCommonLetter(words));
     }
 
-    public static char[] FindingCommonLetter(String[] words) {
+    public static char[] findingCommonLetter(String[] words) {
         String a = words[0];
-        String b ="";
+        String b = "";
         for (int i = 0; i < a.length(); i++) {
             int c = 0;
             for (int j = 1; j < words.length; j++) {
-                if (words[j].contains(a.substring(i, i + 1))) {
-                    words[j]=words[j].replaceFirst(a.substring(i, i + 1),"");
+                if (words[j].contains(a.charAt(i) + "")) {
+                    words[j] = words[j].replaceFirst(a.charAt(i) + "", "");
                     c++;
                 }
             }
-            if(c>= words.length-1) b=b.concat(a.substring(i, i + 1));
+            if (c >= words.length - 1) b = b.concat(a.charAt(i) + "");
         }
         return b.toCharArray();
     }
+
 }
