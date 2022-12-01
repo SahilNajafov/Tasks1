3c3d4f14d786b018e24f7fadd609dd841d4fab3a
SahilNajafov <101974628+SahilNajafov@users.noreply.github.com>
2022-12-01 19:25:10 +0400

    update23

---
+++ src/az/company/task/genericPractic/safdgsfahd.java
@@ -0,0 +1,11 @@
+package az.company.task.genericPractic;
+
+public class safdgsfahd {
+    public static <T> T ekranaYazdir(Object o){
+        return (T)o;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((String) ekranaYazdir(45));
+    }
+}
