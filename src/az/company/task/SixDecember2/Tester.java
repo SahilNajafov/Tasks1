f8ab14074e2ed1e97e4a53fd30d85ada08c8d3bb
SahilNajafov <101974628+SahilNajafov@users.noreply.github.com>
2022-12-06 19:20:32 +0400

    update29

---
+++ src/az/company/task/SixDecember2/Tester.java
@@ -0,0 +1,14 @@
+package az.company.task.SixDecember2;
+
+public class Tester extends Employeer{
+    public Tester(Object name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Tester{" +
+                "name=" + name +
+                '}';
+    }
+}
