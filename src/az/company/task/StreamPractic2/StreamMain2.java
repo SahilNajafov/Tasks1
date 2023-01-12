df43a034a200abc785c534c3950d798bc68373cd
SahilNajafov <101974628+SahilNajafov@users.noreply.github.com>
2023-01-12 20:26:46 +0400

    update47

--- src/az/company/task/StreamPractic2/StreamMain2.java
+++ src/az/company/task/StreamPractic2/StreamMain2.java
@@ -6,6 +6,13 @@ import java.util.stream.IntStream;
 public class StreamMain2 {
     public static void main(String[] args) {
 
-        Boolean ints = new Random().ints(0,100).peek(System.out::println).anyMatch(n->n==45);
+//        Boolean ints = new Random().ints(0,100).peek(System.out::println).anyMatch(n->n==45);
+
+
+        IntStream.iterate(0, i -> i + 1).limit(6)
+                .peek(System.out::println)
+                .mapToDouble(i->Math.pow(i,i-1)).forEach(System.out::println);
+
+
     }
 }
