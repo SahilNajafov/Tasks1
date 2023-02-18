467955c8c1c030d2ea51c0889b14153540e992d6
SahilNajafov <101974628+SahilNajafov@users.noreply.github.com>
2023-02-19 01:56:56 +0400

    update63

---
+++ src/az/company/task/JavaFiles/FileHomeWork.java
@@ -0,0 +1,48 @@
+package az.company.task.JavaFiles;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class FileHomeWork{
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> dataList = new ArrayList<>();
+
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\IntellijIDEA\\Tasks1\\src\\az\\company\\task\\JavaFiles\\secret_message.txt"));
+
+        String line = reader.readLine();
+        int i = 0;
+        while (line != null) {
+            dataList.add(line);
+            line = reader.readLine();
+            i++;
+        }
+
+        reader.close();
+
+        String[] array = dataList.toArray(new String[i]);//new String[0] ???????????????????????????????
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        List<LocalDateTime> datesList = new ArrayList<>();
+        for (int k = 0; k < i; k++) {
+            LocalDateTime xd = LocalDateTime.parse(array[k], dateTimeFormatter);
+            datesList.add(xd);
+        }
+        Collections.sort(datesList);
+//        datesList.forEach(System.out::println);
+
+        for (LocalDateTime b: datesList) {
+            System.out.println(b.format(dateTimeFormatter));
+        }
+
+    }
+
+}
+
